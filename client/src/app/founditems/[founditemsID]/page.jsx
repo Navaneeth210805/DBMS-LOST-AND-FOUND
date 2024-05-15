7e327d31ec5ef1075bd40ee324f48177a7897b73
Navaneethakrishnan R <navaneethakrishnan2108@gmail.com>
2024-05-15 13:02:57 +0530

    Added filters to both lost and found items page

--- client/src/app/founditems/[founditemsID]/page.jsx
+++ client/src/app/founditems/[founditemsID]/page.jsx
@@ -3,13 +3,21 @@ import React, { useState, useEffect } from 'react'
 import Navbar from '@/app/(components)/navbar';
 import axios from 'axios';
 import FoundCard from '@/app/(components)/foundcard';
+import { useSearchParams } from 'next/navigation';
 
 const FoundItems = ({params}) => {
+    const searchparams = useSearchParams()
+    const search1 = searchparams.get("rollno");
+    const search2 = searchparams.get('item');
+    const search3 = searchparams.get('date');
+    const [rollsearching , setsearch] = useState("null")
+    const [itemsearching,setitem] = useState("null")
+    const [datesearching,setdate] = useState("null")
     const [foundItems, setFoundItems] = useState([]);
     useEffect(() => {
-        const fetchFoundItems = async () => {
+        const fetchFoundItems = async (rollsearching,itemsearching,datesearching) => {
             try {
-                const response = await axios.get('http://127.0.0.1:8080/api/found_items');
+                const response = await axios.get(`http://127.0.0.1:8080/api/found_items?rollno=${rollsearching}&item=${itemsearching}&date=${datesearching}`);
                 console.log('Fetched data:', response.data.found_item);
                 setFoundItems(response.data.found_item);
             } catch (error) {
@@ -17,12 +25,60 @@ const FoundItems = ({params}) => {
             }
         };
 
-        fetchFoundItems();
+        fetchFoundItems(search1,search2,search3);
     }, []);
 
+    const handlesubmit = () =>{
+        const href=`/founditems/${params.founditemsID}?rollno=${rollsearching}&item=${itemsearching}&date=${datesearching}`
+        window.location.href=href;
+      }
 
     return (
         <div className=''>
+            <div>
+            {rollsearching=="null" ?
+                (<input type="text"
+                value =""
+                onChange={(e)=>setsearch(e.target.value)} 
+                placeholder='Search for rollno'       
+            />):
+                (<input type="text"
+                value ={rollsearching}
+                onChange={(e)=>setsearch(e.target.value)} 
+                placeholder='Search for rollno'       
+                />)
+            }
+            
+            </div>
+            <div>
+            {itemsearching=="null" ?
+                (<input type="text"
+                value =""
+                onChange={(e)=>setitem(e.target.value)} 
+                placeholder='Search for item'       
+            />):
+                (<input type="text"
+                value ={itemsearching}
+                onChange={(e)=>setitem(e.target.value)} 
+                placeholder='Search for item'       
+                />)
+            }
+            </div>
+
+            <div>
+                <input type="date"
+                value = {datesearching}
+                onChange={(e)=>setdate(e.target.value)}
+                placeholder='Date search'
+                inputMode='numeric'
+                min="1997-01-01"
+                max="2099-12-31"
+                />
+            </div>
+
+            <div>
+                <button onClick={handlesubmit}>Submit</button>
+            </div>
             <Navbar userID = {params.founditemsID}/>
             <h2 className='text-white flex justify-center text-5xl'>Found Items</h2>
             <div className='flex justify-center p-10 mt-28'>
--- client/src/app/lostitems/[lostitemsID]/page.jsx
+++ client/src/app/lostitems/[lostitemsID]/page.jsx
@@ -3,14 +3,24 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from '@/app/(components)/navbar';
 import Card from '@/app/(components)/card';
-
-const LostItems = ( { params } ) => {
+import { redirect, setLazyProp } from 'next/dist/server/api-utils';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+const LostItems = ( { params}) => {
+  const searchparams = useSearchParams()
+  const search1 = searchparams.get("rollno");
+  const search2 = searchparams.get('item');
+  const search3 = searchparams.get('date');
+  const [rollsearching , setsearch] = useState("null")
   const [lostItems, setLostItems] = useState([]);
+  const [itemsearching,setitem] = useState("null")
+  const [datesearching,setdate] = useState("null")
     console.log(params.lostitemsID)
   useEffect(() => {
-    const fetchLostItems = async () => {
+    const fetchLostItems = async (rollsearching,itemsearching,datesearching) => {
+      console.log("Searching :", rollsearching , itemsearching ,datesearching);
       try {
-        const response = await axios.get('http://127.0.0.1:8080/api/lost_items');
+        const response = await axios.post(`http://127.0.0.1:8080/api/lost_items?rollno=${rollsearching}&item=${itemsearching}&date=${datesearching}`);
         console.log('Fetched data:', response.data.lost_item);
         setLostItems(response.data.lost_item);
       } catch (error) {
@@ -18,11 +28,63 @@ const LostItems = ( { params } ) => {
       }
     };
 
-    fetchLostItems();
+    fetchLostItems(search1,search2,search3);
   }, []);
 
+  const handlesubmit = () =>{
+    const href=`/lostitems/${params.lostitemsID}?rollno=${rollsearching}&item=${itemsearching}&date=${datesearching}`
+    window.location.href=href;
+  }
+  
+
   return (
+    
     <div className=''>
+      <div>
+      {rollsearching=="null" ?
+        (<input type="text"
+        value =""
+        onChange={(e)=>setsearch(e.target.value)} 
+        placeholder='Search for rollno'       
+      />):
+        (<input type="text"
+        value ={rollsearching}
+        onChange={(e)=>setsearch(e.target.value)} 
+        placeholder='Search for rollno'       
+        />)
+      }
+      
+      </div>
+      <div>
+      {itemsearching=="null" ?
+        (<input type="text"
+        value =""
+        onChange={(e)=>setitem(e.target.value)} 
+        placeholder='Search for item'       
+      />):
+        (<input type="text"
+        value ={itemsearching}
+        onChange={(e)=>setitem(e.target.value)} 
+        placeholder='Search for item'       
+        />)
+      }
+      </div>
+
+      <div>
+        <input type="date"
+         value = {datesearching}
+         onChange={(e)=>setdate(e.target.value)}
+         placeholder='Date search'
+         inputMode='numeric'
+         min="1997-01-01"
+         max="2099-12-31"
+         />
+      </div>
+
+      <div>
+          <button onClick={handlesubmit}>Submit</button>
+      </div>
+
       <Navbar userID = {params.lostitemsID}/>
       <h2 className='text-white flex justify-center text-5xl'>Lost Items</h2>
       <div className='flex justify-center p-10 mt-28'>
